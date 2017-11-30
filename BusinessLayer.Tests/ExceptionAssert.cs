212dbae51f6f4efee1645b8272e7b03b8260bc17
VSW\nomicaug <augustinas.nomicas@visma.com>
2017-11-30 08:52:49 +0200

    Initial commit

---
+++ BusinessLayer.Tests/ExceptionAssert.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace BusinessLayer.Tests
+{
+	public static class ExceptionAssert
+	{
+		public static T Throws<T>(Action action) where T : Exception
+		{
+			try
+			{
+				action();
+			}
+			catch (T ex)
+			{
+				return ex;
+			}
+
+			Assert.True(false, string.Format("Expected exception of type {0}.", typeof(T)));
+
+			return null;
+		}
+	}
+}
